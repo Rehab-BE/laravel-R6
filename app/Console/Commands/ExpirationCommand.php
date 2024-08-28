b871211e913d81175defe07212460685e480f826
Rehab-BE <rehabadal202@gmail.com>
2024-08-28 03:02:00 +0200

    task15

---
+++ app/Console/Commands/ExpirationCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class ExpirationCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:expiration-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        User::where("expired", '=',0)->update(['expired' => 1]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('admin');
+});
 
 // Route::get('cv', function () {
 //     return view('cv');
